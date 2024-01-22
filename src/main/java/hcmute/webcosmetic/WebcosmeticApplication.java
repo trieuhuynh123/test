591bb6d605541dae65c6ce7a7919c7c3b5f4e05c
Triehuynh123 <huynh3896@gmail.com>
2024-01-22 11:38:15 +0700

    first

---
+++ src/main/java/hcmute/webcosmetic/WebcosmeticApplication.java
@@ -0,0 +1,13 @@
+package hcmute.webcosmetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcosmeticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebcosmeticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hcmute/webcosmetic/WebcosmeticApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.webcosmetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcosmeticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
